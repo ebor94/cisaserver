0fa32800b1aceec20a93be2247c8615bd7b4a1d3
ebor94 <webmaster@ceramicaitalia.com>
2024-10-21 09:59:56 -0500

    ajuste middleware para validacion de usuario

--- midleware/validaRuta.js
+++ midleware/validaRuta.js
@@ -5,6 +5,7 @@ import { verifyAcces } from "../db/login/index.js";
 export const validarRutaUsuario = async (req, res, next) => {
     const userId = req.headers['user-id']; // Supongamos que el ID del usuario viene en los headers
     const app    = req.headers['app']; // Supongamos que el ID del usuario viene en los headers
+    console.log(req)
     if (!userId) {
       return res.status(400).send({ error: 'Usuario no proporcionado' });
     }
@@ -15,7 +16,7 @@ export const validarRutaUsuario = async (req, res, next) => {
       console.log(usuario)
   
       if (usuario[0].INGRESA == 0) {
-        return res.status(404).send({ error: 'Usuario no encontrado' });
+        return res.status(404).send({ error: 'No tiene Acceso a esta aplicacion' });
       }
   
       // Si el usuario existe, continua con la siguiente función (async de la ruta)

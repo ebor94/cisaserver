55bc4640860494b3c22bb8b36ca33f807df1efed
ebor94 <ebor94@hotmail.com>
2024-10-24 10:00:51 -0500

    function GetRoturaController

--- controllers/producto/producto.js
+++ controllers/producto/producto.js
@@ -1,3 +1,4 @@
+import { GetRotura } from '../../services/sap/inventario.js';
 import {GetPrice, SamplePortfolio} from '../../services/sap/product.js'
 
 
@@ -13,4 +14,9 @@ export const GetSamples = async(data) =>{
     let sampleList = await SamplePortfolio(data);
     return sampleList;
 
+}
+
+export const GetRoturaController = async({centro, mov1, mov2, fechaini, fechafi}) =>{
+    let indicadorRotura = await GetRotura(centro, mov1, mov2, fechaini, fechafi);
+    return indicadorRotura;
 }

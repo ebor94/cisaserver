5d778a56685dd17d96dacd21478ce6886ae890d1
ebor94 <ebor94@hotmail.com>
2024-10-24 10:47:17 -0500

    route docs  getRotura

--- routes/producto/product.js
+++ routes/producto/product.js
@@ -20,7 +20,63 @@ router.post(process.env.RUTA_PRODUCT_PRICE,  (req,res) => {
    });    
     
   });
-
+/**
+ * @swagger
+ * 
+ * /producto/rotura/{centro}/{mov1}/{mov2}/{fechaini}/{fechafin}:
+ *   get:
+ *     summary: Get indicator product break only warehouse and type transaction between date's
+ *     tags:
+ *       - Producto # Categoría o sección donde aparecerá esta ruta
+ *     parameters:
+ *       - in: path
+ *         name: centro
+ *         required: true
+ *         value: '1100'
+ *         description: wareHouse
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: mov1
+ *         required: true
+ *         value: '551'
+ *         description: type transaction
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: mov2
+ *         required: true
+ *         value: '552'
+ *         description: type transaction
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: fechaini
+ *         required: true
+ *         value: '20240101'
+ *         description: start Date
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: fechafin
+ *         required: true
+ *         value: '20241130'
+ *         description: End Date
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: return wareHouse and Quantity product break 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 cedula:
+ *                   type: string
+ *                 nombre:
+ *                   type: string
+ */
   router.get(process.env.RUTA_GET_ROTURA,(req,res)=>{
     let centro = req.params.centro;
     let mov1 = req.params.mov1;

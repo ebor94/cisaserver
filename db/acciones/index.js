0c77ef772f435aede550f84b5674824151f9f2fa
ebor94 <ebor94@hotmail.com>
2024-11-01 10:01:21 -0500

    add procedures save_actions

--- db/acciones/index.js
+++ db/acciones/index.js
@@ -27,7 +27,109 @@ async function getPool() {
       throw err;
     }
   };
-  
+
+  export const Save_Action_Value = async (codDestinatario,codTipoDoc,docNo,txtvalor,accion,usuario,seleccion,observacion) => {
+    try {
+      const pool = await getPool();
+      const result = await pool.request()
+        .input('CodDestinatario', sql.VarChar, codDestinatario)
+        .input('CodTipoDoc', sql.VarChar, codTipoDoc)
+        .input('DocNo', sql.VarChar, docNo)
+        .input('txtvalor', sql.VarChar, txtvalor)
+        .input('accion', sql.VarChar, accion)
+        .input('usuario', sql.VarChar, usuario)
+        .input('seleccion', sql.VarChar, seleccion)
+        .input('observacion', sql.VarChar, observacion)
+        .execute('Grabar_Accion_Valor');
+      
+      return result.recordset;
+    } catch (err) {
+      console.error('Error en Save_Action_Value:', err);
+      throw err;
+    }
+  };
+  export const Save_Action_Date = async (codDestinatario,codTipoDoc,docNo,fecha,accion,usuario) => {
+    //const fecha = new Date().toISOString();
+    try {
+      const pool = await getPool();
+      const result = await pool.request()
+        .input('CodDestinatario', sql.VarChar, codDestinatario)
+        .input('CodTipoDoc', sql.VarChar, codTipoDoc)
+        .input('DocNo', sql.VarChar, docNo)
+        .input('fecha', sql.DateTime, fecha)
+        .input('accion', sql.VarChar, accion)
+        .input('usuario', sql.VarChar, usuario)
+        .execute('Grabar_Accion_Fecha');
+      
+      return result.recordset;
+    } catch (err) {
+      console.error('Error en Save_Action_Date:', err);
+      throw err;
+    }
+  };
+
+  export const Save_Action_Employe = async (codDestinatario,codTipoDoc,docNo,empleado,accion,usuario) => {
+    try {
+      const pool = await getPool();
+      const result = await pool.request()
+        .input('CodDestinatario', sql.VarChar, codDestinatario)
+        .input('CodTipoDoc', sql.VarChar, codTipoDoc)
+        .input('DocNo', sql.VarChar, docNo)
+        .input('empleado', sql.VarChar, empleado)
+        .input('accion', sql.VarChar, accion)
+        .input('usuario', sql.VarChar, usuario)
+        .execute('Grabar_Accion_Empleado');
+      
+      return result.recordset;
+    } catch (err) {
+      console.error('Error en getEmpleadoDb:', err);
+      throw err;
+    }
+  };
+
+  export const Save_Action_List_Value = async (codDestinatario,codTipoDoc,docNo,opcion,accion,usuario) => {
+    try {
+      const pool = await getPool();
+      const result = await pool.request()
+        .input('CodDestinatario', sql.VarChar, codDestinatario)
+        .input('CodTipoDoc', sql.VarChar, codTipoDoc)
+        .input('DocNo', sql.VarChar, docNo)
+        .input('opcion', sql.VarChar, opcion)
+        .input('accion', sql.VarChar, accion)
+        .input('usuario', sql.VarChar, usuario)
+        .execute('Grabar_Accion_Lista_Valor');
+      
+      return result.recordset;
+    } catch (err) {
+      console.error('Error en Grabar_Accion_Lista_Valor:', err);
+      throw err;
+    }
+  };
+
+
+  export const Save_Action_List_every_Value = async (codDestinatario,codTipoDoc,docNo,opcion,accion,usuario,insertar) => {
+    try {
+      const pool = await getPool();
+      const result = await pool.request()
+        .input('CodDestinatario', sql.VarChar, codDestinatario)
+        .input('CodTipoDoc', sql.VarChar, codTipoDoc)
+        .input('DocNo', sql.VarChar, docNo)
+        .input('opcion', sql.VarChar, opcion)
+        .input('accion', sql.VarChar, accion)
+        .input('usuario', sql.VarChar, usuario)
+        .input('insertar', sql.Int , insertar)
+        .execute('Grabar_Accion_ListaMultiple_Valor');
+      
+      return result.recordset;
+    } catch (err) {
+      console.error('Error en Save_Action_List_every_Value:', err);
+      throw err;
+    }
+  };
+
+
+
+
   // Función para cerrar la conexión cuando la aplicación se cierre
   export const closePool = async () => {
     if (pool) {

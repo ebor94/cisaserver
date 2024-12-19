b1d824111af0c27a7d42abe881c18a65dc0f0e74
ebor94 <ebor94@hotmail.com>
2024-12-19 08:22:38 -0500

    add txtcab and name product api

--- controllers/producto/producto.js
+++ controllers/producto/producto.js
@@ -1,4 +1,5 @@
 import { GetRotura } from '../../services/sap/inventario.js';
+import { GetName } from '../../services/sap/product.js';
 import {GetPrice, infoPallet, SamplePortfolio} from '../../services/sap/product.js'
 
 
@@ -18,7 +19,22 @@ export const GetSamples = async(data) =>{
 
 export const GetRoturaController = async(centro,mov1,mov2,fechaini,fechafin,bandera) =>{
     let indicadorRotura = await GetRotura(centro, mov1, mov2, fechaini, fechafin,bandera);
-    return indicadorRotura;
+    for (const material of indicadorRotura) {
+      const { matnr } = material;
+      if (matnr) {
+        try {
+          const [nameData] = await GetName(matnr, 'S')
+
+          material.nombre = nameData.eMaktx;
+        } catch (error) {
+          console.error(`Error al obtener el nombre del material ${matnr}:`, error);
+        }
+      }
+    
+}
+
+return indicadorRotura;
+
 }
 
 export const getInfoPallet = async(pallet,lote,material)=>{

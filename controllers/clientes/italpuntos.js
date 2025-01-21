b6f8e83666b968b7883c279280fb8f7379645860
ebor94 <ebor94@hotmail.com>
2025-01-21 09:56:29 -0500

    add service apprecio and controller

---
+++ controllers/clientes/italpuntos.js
@@ -0,0 +1,26 @@
+import { cuponList } from "../../services/appPrecio"
+
+
+
+export const  getCuponList = async  () => {
+    try {
+        const response = await cuponList();
+        if (!response || response.length === 0) {
+            return {
+              success: false,           
+              data: null
+            };
+          }      
+          return {
+            success: true,        
+            data: response
+          };
+    } catch (error) {
+      //console.error("Error en getInfoPlaca:", error);
+      return {        
+        success: false,         
+        error: error.message
+      };
+
+    }
+}

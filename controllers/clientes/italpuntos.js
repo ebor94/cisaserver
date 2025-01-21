cc3be12ca3b72da27c36165f978de906c8d32e64
ebor94 <ebor94@hotmail.com>
2025-01-21 16:46:42 -0500

    add controller  cuponList

--- controllers/clientes/italpuntos.js
+++ controllers/clientes/italpuntos.js
@@ -1,34 +1,6 @@
 import { cuponList } from "../../services/appPrecio"
 
 
-/**
- * @swagger
- * /italpuntos/CuponList/:
- *   post:
- *     summary: lista de cupones de italpuntos
- *     description: obtiene la lista de cupones de italpuntos atraves de la api de apprecio
- *     tags:
- *       - Italpuntos
- *     responses:
- *       200:
- *         description: Respuesta exitosa
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   description: Indica si la operación fue exitosa
- *                 message:
- *                   type: string
- *                   description: Mensaje descriptivo del resultado
- *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *                   description: Array con los datos de respuesta
- */
 export const  getCuponList = async  () => {
     try {
         const response = await cuponList();

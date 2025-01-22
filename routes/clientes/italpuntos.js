ad26a69812b8d0b6e6295113fbf732dc39112aee
ebor94 <ebor94@hotmail.com>
2025-01-22 08:00:11 -0500

    add route italpuntos

---
+++ routes/clientes/italpuntos.js
@@ -0,0 +1,36 @@
+import { getCuponList, getCuponList } from "../../controllers/clientes/italpuntos"
+/**
+ * @swagger
+ * /italpuntos/CuponList/:
+ *   post:
+ *     summary: lista de cupones de italpuntos
+ *     description: obtiene la lista de cupones de italpuntos atraves de la api de apprecio
+ *     tags:
+ *       - Italpuntos
+ *     responses:
+ *       200:
+ *         description: Respuesta exitosa
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indica si la operación fue exitosa
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje descriptivo del resultado
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                   description: Array con los datos de respuesta
+ */
+router.post('italpuntos/CuponList' , async(req,res)=>{    
+   let response  = await  getCuponList();
+   res.send(response); 
+});
+
+
+export default router

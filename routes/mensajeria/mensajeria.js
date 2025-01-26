e37e83340727f6080171a0ba6b864ef7a8d2782a
ebor94 <ebor94@hotmail.com>
2025-01-25 20:08:14 -0500

    add docs route

--- routes/mensajeria/mensajeria.js
+++ routes/mensajeria/mensajeria.js
@@ -22,7 +22,72 @@ router.post(process.env.RUTA_MENSAJERIA ,  (req,res) => {
      res.send(response);
    });
   });
-
+/**
+ * @swagger
+ * /mensajeria/sms/:
+ *   post:
+ *     summary: envio sms
+ *     description: envio sms
+ *     tags:
+ *       - Mesajeria
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [numero sms]
+ *             properties:
+ *               numero:
+ *                 type: string
+ *                 example: "3165217418"
+ *               sms:
+ *                 type: string
+ *                 example: "prueba sms"
+ *     responses:
+ *       200:
+ *         description: Respuesta exitosa
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indica si la operación fue exitosa
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje descriptivo del resultado
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                   description: Array con los datos de respuesta
+ *       400:
+ *         description: Solicitud incorrecta (Bad Request)
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *       401:
+ *         description: Autenticación fallida (Unauthorized)
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ */
   router.post('/mensajeria/sms/', (req,res)=>{
     const data = req.body;   
     SendSms(data).then((response)=>{ 

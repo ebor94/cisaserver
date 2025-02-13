8490fb2c8b93ce8fb8d614435cf06291e9396a43
ebor94 <ebor94@hotmail.com>
2025-02-13 11:43:19 -0500

    se agrega modifica funcion de envio sms por si falla la de claro se envia por labsmobile

--- controllers/mensajeria/mensajeria.js
+++ controllers/mensajeria/mensajeria.js
@@ -1,5 +1,6 @@
 import { Claro } from "../../services/claro/index.js";
 import { SendMessageGoogle } from "../../services/Gpc/notificacionChat.js";
+import { labsmobile } from "../../services/labsmobile/index.js";
 import { sendMessageWhatsapp } from "../../services/whatsapp/1msg.js"
 
 
@@ -19,23 +20,63 @@ export const sendMessageChatGoogle = async ({message,param1,key ,token})=>{
 }
 
 export const SendSms = async ({phoneNumber, message}) =>{
+  let envio = false
     try {
-        let response = await  Claro.sendSms(phoneNumber,message);       
+        let response = await  Claro.sendSms(phoneNumber,message);  
+           console.log(response);     
         if (!response || response.length === 0) {
+          envio = false
           return {
             success: false,           
             data: null
           };
         }
+
+        if(response.resultCode !== '0'){
+          let response = await labsmobile.sendSms(message,phoneNumber )
+          if(response.code == '0'){
+            envio = true
+            return {
+              success: true,        
+              data: response
+            };
+           }else{
+            envio = false
+            return {
+              success: false,        
+              data: response
+            };
+           }
+        }
+        envio = true
         return {
           success: true,        
           data: response
         };
       } catch (error) {
+        envio = false
         return {
           success: false,         
           error: error.message
         };
+      }finally{
+        if(envio == false){
+          let response = await labsmobile.sendSms(message,phoneNumber )
+          if(response.code == '0'){
+            envio = true
+            return {
+              success: true,        
+              data: response
+            };
+           }
+           return {
+            success: false,        
+            data: response
+          };
+
+
+
+        }
       }
  
     

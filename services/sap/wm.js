2bf7891999b5886f832fa13405773b8f2994f4fa
ebor94 <ebor94@hotmail.com>
2025-03-03 10:30:45 -0500

    add fuction object mt for alistamiento ingreso

--- services/sap/wm.js
+++ services/sap/wm.js
@@ -2,7 +2,7 @@ import  axios from 'axios';
 import dotenv from 'dotenv'
 dotenv.config()
 
-const sapDomain = "https://itaprd.ceramicaitalia.com:44301/sap/bc/rest/"
+const sapDomain =  process.env.SAP_DOMAIN_REST
 
 export const loginWm = (usuario, contraseña, bandera) => {
     let data = JSON.stringify({
@@ -298,4 +298,106 @@ export const GetWeightDelivery = (entrega) =>{
   
       return response; 
 
+}
+
+
+export const mt = {
+    async getInfoMT(codMt,centro){
+        let config = {
+        
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: `${sapDomain}zwm_alist_ing?sap-client=310&vbeln=${codMt}&werks=${centro}`,
+            headers: { 
+                'Content-Type': 'application/json', 
+                'Authorization': process.env.SESSIONWM_AUTORIZATION , 
+                'Cookie': process.env.SESSIONWM_COOKIE
+            },
+          };
+          
+          const response =   axios.request(config)
+          .then((response) => {
+              return response.data;
+          })
+          .catch((error) => {
+              return JSON.stringify(error);
+          });
+      
+          return response;
+    },
+
+    async getInfoIngreso(Consecutivo,Estado,Werks,Lgort,Vbeln){
+
+        try {  
+            // Construir la URL dinámicamente con los parámetros  
+            const baseUrl = `${process.env.SAP_DOMAIN_ODATA}ZWM_ALISTAMIENTO_SRV/BodyEtiqSet`;  
+            const filters = [  
+              Consecutivo ? `Consecutivo eq '${Consecutivo}'` : null,  
+              Estado ? `Estado eq '${Estado}'` : null,  
+              Werks ? `Werks eq '${Werks}'` : null,  
+              Lgort ? `Lgort eq '${Lgort}'` : null,  
+              Vbeln ? `Vbeln eq '${Vbeln}'` : null,  
+            ]  
+              .filter(Boolean) // Eliminar filtros nulos  
+              .join(' and '); // Unir los filtros con "and"  
+          
+            const url = `${baseUrl}?$filter=${filters}&$format=json`;  
+          
+            // Configuración de la solicitud  
+            const config = {  
+              method: 'get',  
+              maxBodyLength: Infinity,  
+              url: url,  
+              headers: {  
+                Authorization: 'Basic Ym9ydGVnYTpCb3J0ZWdhKzExMw==',  
+                Cookie: 'SAP_SESSIONID_CSQ_310=TIU_-oIogSbv4aXpVA4lEBkxk6_4NBHvvY3ncTKwTus%3d; sap-usercontext=sap-client=310',  
+              },  
+            };  
+          
+            // Realizar la solicitud  
+            const response = await axios.request(config);  
+          
+            // Validar que la respuesta contenga datos  
+            if (response.data && response.data.d && response.data.d.results && response.data.d.results.length > 0) {  
+              const { __metadata, AlistamientoIngreso, ...datosLimpios } = response.data.d.results[0];  
+              return datosLimpios; // Retornar los datos limpios  
+            } else {  
+              throw new Error('No se encontraron datos en la respuesta.');  
+            }  
+          } catch (error) {  
+            // Manejo de errores  
+            console.error('Error al obtener la información de ingreso:', error.message);  
+            return { error: error.message };  
+          }
+    },
+    async  registrarIngreso(data) {  
+        try {  
+          // Configuración de la solicitud  
+          const config = {  
+            method: 'post',  
+            maxBodyLength: Infinity,  
+            url: `${process.env.SAP_DOMAIN_ODATA}ZWM_ALISTAMIENTO_SRV/AlistamientoIngresoSet`,  
+            headers: {  
+              'X-CSRF-Token': 'YRMHZZGth2rDJOr5xhFzdg==',  
+              'Content-Type': 'application/json',  
+              'Authorization': 'Basic Ym9ydGVnYTpCb3J0ZWdhKzExMw==',  
+              'Cookie': 'SAP_SESSIONID_CSQ_310=TIU_-oIogSbv4aXpVA4lEBkxk6_4NBHvvY3ncTKwTus%3d; sap-usercontext=sap-client=310',  
+            },  
+            data: JSON.stringify(data), // Convertir el objeto data a JSON  
+          };  
+        
+          // Realizar la solicitud  
+          const response = await axios.request(config);  
+        
+          // Limpiar la respuesta  
+          const { __metadata, BodyEtiqSet, ...datosLimpios } = response.data.d;  
+        
+          // Retornar los datos limpios  
+          return datosLimpios;  
+        } catch (error) {  
+          // Manejo de errores  
+          console.error('Error al registrar el ingreso:', error.message);  
+          return { error: error.message };  
+        }  
+      }
 }

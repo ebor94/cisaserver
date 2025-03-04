a98fb60252e375cdfad8eb7d19cead660ef48fe5
ebor94 <ebor94@hotmail.com>
2025-03-03 19:00:31 -0500

    change fuction valida token

--- services/jwt/index.js
+++ services/jwt/index.js
@@ -5,20 +5,35 @@ console.log("++++")
 
 let token = jwt.sign({
     data: req
-  }, '*963.', { expiresIn: '1h' });
+  }, '*963.', { expiresIn: '5h' });
 
   return token
 }
 
 
-export const validateToken =(token)=>{
-
-    try {
-        let decoded = jwt.verify(token, '*963.');
-        return true 
-      } catch(err) {
-        return false
-      }
+export const validateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
   
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ success: false, message: 'Token no proporcionado' });
+  }
+  
+  // Extraer el token
+  const token = authHeader.split(' ')[1];
+  
+  try {
+    // Verificar el token
+    const decoded = jwt.verify(token, '*963.');
+    
+    // Añadir la información del usuario al request
+    req.usuario = decoded;
+    
+    // Continuar con la solicitud
+    next();
+  } catch (error) {
+    return res.status(401).json({ success: false, message: 'Token inválido' });
+  }
+
+    
     //return decoded
 }
